931387a23ebeb0af8ee99834784b0bdefa374450
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-10-07 17:29:10 +0530

    Refactor environment variables and constants
    - Updated the twoFactorAuth.service.js by using speakeasy library to have generatesecret function to generate 2FA key, verifyToken function to verify otp generated, & generateOTPURL function to create otppathurl for displaying qr in frontend.
    
    - Updated the .env file to include new environment variables for the server, MongoDB, JWT, nodemailer, and Twilio.
    - Modified the DB_NAME constant in the constant.js file to 'mydbs' instead of 'mydb'.
    - Added a new function otpSendText to the constant.js file for sending OTP via text message.
    - Updated the sendMessage function in the message.service.js file to use the Twilio library for sending messages.
    - Refactored the twoFactorAuth.service.js file to improve code readability and maintainability.

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -1,15 +1,23 @@
+import twilio from 'twilio';
 
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
 
- export const sendMessage = (phoneNumber,otp)=>{
 
-    if (phoneNumber && otp) {
-        console.log(otp + " sent to " + phoneNumber);
-        return true
-    }else{
-        return false
+// sends otp to the user
+// to is the user's phone number, message is the Text(otp)
+export const sendMessage = async (to, message) => {
+    try {
+        const response = await client.messages.create({
+            body: message,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to
+        });
+        console.log(response);// we dont have twilio setup so we will test for response later and then remove console.log
+        return response;
+    } catch (error) {
+        console.error('Error sending message:', error);
     }
-
-}
-
- 
+};
--- src/services/twoFactorAuth.service.js
+++ src/services/twoFactorAuth.service.js
@@ -4,30 +4,27 @@ import speakeasy from 'speakeasy';
 // this returns a secret key that we will use to generate the QR code & onces the user enters the token, we will use this secret key to verify the token, after which we save it as 2fa in user model
 export const generateTwoFactorAuth = () => {
         const secret = speakeasy.generateSecret();
-        console.log(secret);
         return secret;
-        }
+}
 // Verify the token
 // secret is the secret key we get from generateTwoFactorAuth, token is the token the user enters
 export const verifyTwoFactorAuth = (secret, token) => {
         const verified = speakeasy.totp.verify({
-            secret: secret,
-            encoding: 'base32',
-            token: token,
-            window: 1
+                secret: secret,
+                encoding: 'base32',
+                token: token,
+                window: 1
         });
-        console.log(verified);
         return verified;
-        }
+}
 // Generate a URL for the QR code
 // secret we get from generateTwoFactorAuth, username is the user's email, issuer is the name of the our app "NTSMETRICS"
 
 export const generateTwoFactorAuthUrl = (secret, username, issuer) => {
         const otpauth_url = speakeasy.otpauthURL({
-            secret,
-            label: username,
-            issuer: issuer
+                secret,
+                label: username,
+                issuer: issuer
         });
-        console.log(otpauth_url);
         return otpauth_url;
-        }
+}
